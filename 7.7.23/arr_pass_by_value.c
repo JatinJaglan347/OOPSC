0b4ae4e3736499551947a0b381198c516918aa37
JatinJaglan347 <jatinjaglan347@gmail.comm>
2023-07-07 09:29:38 +0530

    7.7.2023

---
+++ 7.7.23/arr_pass_by_value.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void update(int arr [] , int n){
+typedef struct array {
+    int arr [ 10];
+    int n = 10;
+
+}arr;
+}
+int main (){
+    int arr [] = {1,2,3,4,5};
+    int n = sizeof (arr)/sizeof (arr[0]);
+    update (arr , n );
+    for( int i = 0 ; i <n ; i ++){
+        printf ("%d", arr [i]);
+    }
+}
+
